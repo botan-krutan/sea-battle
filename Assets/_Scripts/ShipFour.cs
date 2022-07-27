d33f02c2366e2ddfeeaba406baabbc0318fd49ae
botan-krutan <72577957+botan-krutan@users.noreply.github.com>
2022-07-27 12:02:46 +0300

    ships update

--- Assets/_Scripts/ShipFour.cs
+++ Assets/_Scripts/ShipFour.cs
@@ -7,12 +7,17 @@ public class ShipFour : ShipBase
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = 4;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public override void AddOffset()
+    {
+        base.AddOffset();
+        gameObject.transform.Translate(0, 2, 0);
     }
 }
--- Assets/_Scripts/ShipThree.cs
+++ Assets/_Scripts/ShipThree.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipThree : ShipBase
-{
+{ 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public override void AddOffset()
+    {
+        base.AddOffset();
+        gameObject.transform.Translate(0, 1, 0);
     }
 }
